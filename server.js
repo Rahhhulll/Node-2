6324c61cf643b827cd8d7204064ae70845996fda
Rahhhulll <rkrahulrk651@gmail.com>
2022-05-22 10:06:16 +0530

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
    
      res.send(`<h1> Ex.2 : Create a server using express </h1>
      <p> Ques.1 Send JSON response, a student details like name, age, email, contact, course, date of joining.</p>
-     <b> Ans : { Name: Abhay, Age: 24, Email: abhaygangwar0807@gmail.com, Contact: 8859888436, Course: Full Stack Web Developer,  Date of Joining : December 2021} </b>`);
+     <b> Ans : { Name:Rahul, Age: 23, Email: rkrahulrk651@gmail.com, Contact: 9096161097, Course: MERN STACK Web Developer,  Date of Joining : Sup 2021} </b>`);
     
 
     res.end()
